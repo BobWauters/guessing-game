886bdcb904a463de94916916430bee52009b2b39
BobW <biz.wautersbob@gmail.com>
2021-06-16 15:25:47 +0200

    Add User Guess functionality

--- script.js
+++ script.js
@@ -10,6 +10,8 @@
     console.log(getRandomInt(15));
     // expected output: 1 to 15
 
+    //variables for secret and user number
+    const secretNumber = getRandomInt(15)
 // TODO: way for users to guess (also html)
 
     document.getElementById('guessUserLabel').innerHTML ='Guess the right number between 1 - 15 to win';
@@ -17,13 +19,14 @@
 // TODO: capture click of button
     // On click
     document.getElementById('userGuessButton').addEventListener('click', function() {
-        var guessUser = Number (document.getElementById(userGuess))
-    document.getElementById('userGuess').value;
+    const userGuess = document.getElementById('userGuess').value;
+    console.log(userGuess);
     });
 
     //event-listener
 
 // TODO: compare guess to secret number
+    if (userGuess ==secretNumber);
 // TODO: end message show
 
 
