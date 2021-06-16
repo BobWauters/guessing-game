b3f614afe05cacbe797611f25738853a2a4fc244
BobW <biz.wautersbob@gmail.com>
2021-06-16 12:30:48 +0200

    Generate numbers 1-15

---
+++ script.js
@@ -0,0 +1,22 @@
+
+(function (){
+    console.log('test excercise')
+
+// TODO: generate numbers 1-15
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    console.log(getRandomInt(15));
+    // expected output: 0 to 15
+
+// TODO: way for users to guess
+
+
+// TODO: capture click on play button
+// TODO: compare guess to secret number
+// TODO: end message show
+
+
+
+})();
