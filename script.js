1f96ada4dc4563b31da0d96956c638af2772f9e7
BobW <biz.wautersbob@gmail.com>
2021-06-16 15:17:39 +0200

    Fixed todo button click

--- script.js
+++ script.js
@@ -5,15 +5,24 @@
 // TODO: generate numbers 1-15
 
     function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+        return Math.floor(Math.random() * max) +1;
     }
     console.log(getRandomInt(15));
-    // expected output: 0 to 15
+    // expected output: 1 to 15
 
-// TODO: way for users to guess
+// TODO: way for users to guess (also html)
 
+    document.getElementById('guessUserLabel').innerHTML ='Guess the right number between 1 - 15 to win';
 
-// TODO: capture click on play button
+// TODO: capture click of button
+    // On click
+    document.getElementById('userGuessButton').addEventListener('click', function() {
+        var guessUser = Number (document.getElementById(userGuess))
+    document.getElementById('userGuess').value;
+    });
+
+    //event-listener
+
 // TODO: compare guess to secret number
 // TODO: end message show
 
