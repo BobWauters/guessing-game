a649115c00022c62c3c644ba0b3d4995cba9857d
BobW <biz.wautersbob@gmail.com>
2021-06-17 09:32:12 +0200

    Fix innerHTML null error

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 
-(function (){
+(function () {
     console.log('test excercise')
 
 // TODO: generate numbers 1-15
 
     function getRandomInt(max) {
-        return Math.floor(Math.random() * max) +1;
+        return Math.floor(Math.random() * max) + 1;
     }
-    console.log(getRandomInt(15));
+
+    /*  TRIED FIXING INNERHTML GIVE VALUE
+
+    const peopleInVerou =15;
+    console.log(getRandomInt(peopleInVerou));
+    */
+
+
+    //console.log(getRandomInt(15));
     // expected output: 1 to 15
 
     //variables for secret and user number
     const secretNumber = getRandomInt(15)
+
 // TODO: way for users to guess (also html)
 
-    document.getElementById('guessUserLabel').innerHTML ='Guess the right number between 1 - 15 to win';
+    document.getElementById('guessUserLabel').innerHTML = 'Guess the right number between 1 - 15 to win';
 
 // TODO: capture click of button
-    // On click
-    document.getElementById('userGuessButton').addEventListener('click', function() {
-    const userGuess = document.getElementById('userGuess').value;
-    console.log(userGuess);
-    });
+    // event-listener
+    document.getElementById('userGuessButton').addEventListener('click', function () {
+        const userGuess = document.getElementById('userGuess').value;
+        console.log(userGuess);
 
-    //event-listener
+
+        //onclick
 
 // TODO: compare guess to secret number
-    if (userGuess ==secretNumber);
+
+        console.log(document);
+        console.log(document.getElementById('result'));
+
+        if (userGuess === secretNumber) {
+            document.getElementById('result').innerHTML = 'whoop whoop ya got it right';
+        } else {
+            document.getElementById('result').innerHTML = 'Ya guessed wrong, ya answer be ${userGuess} , the right answer was ';
+        }
+
+
 // TODO: end message show
 
 
-
+    });
 })();
